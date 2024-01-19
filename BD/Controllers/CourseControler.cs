2d5fd5539e5ec7556d628f27d83983ae9e2e3195
m-stepien <mstepien7@protonmail.com>
2024-01-19 22:06:40 +0100

    stworzenie podstaw course

---
+++ BD/Controllers/CourseControler.cs
@@ -0,0 +1,29 @@
+﻿using BD.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BD.Controllers
+{
+    public class CourseControler : Controller
+    {
+            public IActionResult Course()
+            {
+                Course course = new Course
+                {
+                    Id = 1,
+                    Name = "Sample Course",
+                    Price = 29.99m,
+                    Duration = 10,
+                    Author = "John Doe",
+                    Description = "A sample course description.",
+                    Category = "Programming",
+                    Rate = 4.5,
+                    VotesNum = 100
+                };
+
+                return View();
+            }
+        
+    
+}
+}
---
+++ BD/Models/Course.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BD.Models
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+        
+        
+        [Column(TypeName = "int")]
+        public int Duration { get; set; }
+
+
+        [Column(TypeName = "varchar(30)")]
+        public string Author { get; set; }
+
+
+        [Column(TypeName = "TEXT")]
+        public string Description { get; set; }
+        
+        
+        [Column(TypeName = "varchar(30)")]
+        public string Category { get; set; }
+        
+        
+        [Column(TypeName = "int")]
+        public double Rate { get; set; }
+
+        
+        [Column(TypeName = "int")]
+        public int VotesNum { get; set; }
+    }
+}
